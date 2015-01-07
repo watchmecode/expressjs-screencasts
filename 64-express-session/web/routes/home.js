4e5efbf3075a2b6c7b05356e231ab24d161d3f5c
Derick Bailey <derickbailey@gmail.com>
2015-01-07 09:43:35 -0600

    added folder for episode 64 on express session

---
+++ 64-express-session/web/routes/home.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+function addSomething(req, res, next){
+  if (true == false){
+    next();
+  } else {
+    res.render("error");
+  }
+}
+
+function renderHome(req, res, next) {
+  res.render('index', { title: 'Express' });
+}
+
+router.get('/', addSomething, renderHome);
+
+module.exports = router;
